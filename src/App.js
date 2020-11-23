5a0895b113fbd48dc44309541e901574da590622
DESKTOP-SSTRNI0\HOON <lee040118@naver.com>
2020-11-24 00:09:22 +0900

    201124_test1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  //var [a,b] = [10,100];
+  let [문제번호, 문제번호변경] = useState(['1번', '2번']);
+  let [따봉 ,따봉변경]=useState(0);
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>Online Judge 개발중...</div>
+        <div className='login'>
+          <a href="http://naver.com">login</a>
+        </div>
+      </div>
+      <div className='list'>
+        <h4>문제 목록 </h4>
+        <p>{문제번호[1]} <span onClick={ () => {따봉변경(따봉+1)} }> 👍</span> {따봉}</p>
+        <hr/>
+      </div>
     </div>
   );
 }
 
 export default App;
+
